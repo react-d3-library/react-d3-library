ecbfbd84f47dc915dadcdd1a5497d8513afddb14
danieljoonlee <danieljoonlee@gmail.com>
2016-05-06 12:21:43 -0700

    cleaned up sand box to use minimal necessities

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+app.use(express.static('static'));
 
-  console.log('Listening at http://localhost:3000/');
-});
+// render index page
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname + './static'));
+})
+
+app.listen(3000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,20 @@
-var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
+  entry: './client/index.js',
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    filename: './static/js/bundle.js'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
   }
 };
