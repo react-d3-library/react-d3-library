27fa9bbf7a4de110cfc3e03f70fb165d4f0ad723
Dave Loyst <david.loyst@gmail.com>
2016-05-11 12:39:08 -0700

    refactored getStyles to convert to camel case

--- client/getStyles.js
+++ client/getStyles.js
@@ -1,20 +1,26 @@
 module.exports =  styleObject => {
-		// console.log(typeof JSON.parse(styleObject));
-  		var styles = {}
-  		if(typeof styleObject === 'string') {
-  			var index1 = styleObject.indexOf(':');
-  			var index2 = styleObject.indexOf(';')
-  			var key = styleObject.slice(0, index1);
-  			if (key === 'text-anchor') key = "textAnchor"
-  			var value = styleObject.slice(index1 + 2, index2);
-  			styles[key] = value;
-  			console.log('stylesObject: ', styles)
-  		} else {
-	  		for(var key in styleObject) {
-	  			if(!isNaN(key)) {
-	  				styles[styleObject[key]] = styleObject[styleObject[key]];
-	  			} 
-	  		}
-	  	}	
-  		return styles;
+	const styles = {};
+
+	if(typeof styleObject === 'string') {
+    const isDash = styleObject.indexOf('-');
+    const atColon = styleObject.indexOf(':');
+    const subDash = styleObject.slice(0, isDash);
+
+    const key = isDash > -1 
+      ? (styleObject.slice(0, isDash) + styleObject.slice(isDash + 1, isDash + 2).toUpperCase() + styleObject.slice(isDash + 2, atColon))
+      : styleObject.slice(0, atColon);
+
+    const isNum = styleObject.slice(atColon + 2, styleObject.length - 1);
+    const value = isNaN(isNum) ? isNum : Number(isNum);
+
+		styles[key] = value;
+
+	} else {
+		for(var key in styleObject) {
+			if(!isNaN(key)) {
+				styles[styleObject[key]] = styleObject[styleObject[key]];
+			} 
+		}
+	}	
+	return styles;
 }
