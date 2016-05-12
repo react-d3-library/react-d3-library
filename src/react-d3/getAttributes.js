984e0d8f1ddb079e24e1ab024117d769967713cf
Andrew Burke <andrewburke@Andrews-MacBook-Pro.local>
2016-05-12 15:17:08 -0700

    wrote toCamelCase Utility and plugget it into getAttributes

--- src/react-d3/getAttributes.js
+++ src/react-d3/getAttributes.js
@@ -1,8 +1,16 @@
+const toCamelCase = require('./../../utils/toCamelCase');
+
 module.exports = attributesObject => {
 	var attributes = {}
 	for(var key in attributesObject) {
 		if(!isNaN(key)) {
-			attributes[attributesObject[key].localName] = attributesObject[key].nodeValue;
+			if(attributesObject[key].localName === 'class') {
+				attributes['className'] = attributesObject[key].nodeValue;
+			} else if (attributesObject[key].localName.indexOf('-') > -1) {
+				var reactKey = toCamelCase(attributesObject[key].localName);
+				attributes[reactKey] = attributesObject[key].nodeValue;
+			}
+			else attributes[attributesObject[key].localName] = attributesObject[key].nodeValue;
 		}
 	}
 	return attributes;
