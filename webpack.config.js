16c0539e5e298e4a4a4215f75ab276a77b5d2c76
Andrew Burke <andrewburke@Andrews-MacBook-Pro.local>
2016-05-12 14:33:21 -0700

    extracted getAttributes function and cleaned/commented d3DataToJSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,7 @@
 var webpack = require('webpack');
 
 module.exports = {
-<<<<<<< HEAD
-  entry: './client/index.js',
-  plugins: [
-  new webpack.DefinePlugin({
-    // Force HTMLtoJSX to use the in-browser `document` object rather than
-    // require the Node-only "jsdom" package.
-    IN_BROWSER: true,
-
-    // Expose the version to embed in the final file.
-    TURBO_REACT_VERSION: JSON.stringify(require("./package.json").version)
-  })
-  ],
-=======
   entry: './src/demo/index.js',
->>>>>>> 19652cf68bc7b3e5f0e3ef862392a8ca276374d3
   output: {
     filename: './static/js/bundle.js'
   },
